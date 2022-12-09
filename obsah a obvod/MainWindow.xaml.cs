f444e738751f34a8d15c89e904cd5b7355709904
Jan Ptáček <jan.ptacek@educhem.cz>
2022-12-09 10:16:02 +0100

    Přidejte soubory projektu.

---
+++ obsah a obvod/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace obsah_a_obvod
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int a, b, c, d, e;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show("Povrch kvádru:" + vypocet().ToString() + "cm2"
+                + "       Obsah kvadru:" + vypocetp().ToString()+"cm3");
+        }
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            
+        }
+        public int vypocet()
+        {
+
+            a = Convert.ToInt32(A.Text);
+            b = Convert.ToInt32(A.Text);
+            c = Convert.ToInt32(C.Text);
+            d = 2*((a*b)+(a*c)+(b*c));
+            return d;
+        }
+        public int vypocetp()
+        {
+            e = a * b * c;
+            return e;
+
+        }
+
+
+
+        
+
+       
+    }
+}
